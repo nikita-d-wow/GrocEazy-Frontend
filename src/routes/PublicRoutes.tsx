6d410f8ba960cd9d0a45a10323d90201e93ddd4b
arjun-wow <arjun.u@wowlabz.com>
2025-12-18 11:37:25 +0530

    feat: implement forgot and reset password

--- src/routes/PublicRoutes.tsx
+++ src/routes/PublicRoutes.tsx
@@ -2,10 +2,14 @@
 import { Route } from 'react-router-dom';
 import Login from '../views/auth/Login';
 import Register from '../views/auth/Register';
+import ForgotPassword from '../views/auth/ForgotPassword';
+import ResetPassword from '../views/auth/ResetPassword';
 
 export const PublicRoutes = (
   <>
     <Route path="/login" element={<Login />} />
     <Route path="/register" element={<Register />} />
+    <Route path="/forgot-password" element={<ForgotPassword />} />
+    <Route path="/reset-password" element={<ResetPassword />} />
   </>
 );
